1c2b27bdfe7ffaf5e7732a06112dbb9d2a2fcc3c
Devika Byeleholi <devika@example.com>
2025-06-17 12:53:28 +0530

    Fix Dockerfile: Hardcode port 5000

--- app.py
+++ app.py
@@ -223,8 +223,7 @@ def admin_login():
 
 
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
+
 
 
